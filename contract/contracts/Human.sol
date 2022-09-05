6001576adb04b0d9725375dfb321aa1d63792bb9
peter anyaogu <anyaogupeter601@gmail.com>
2022-09-05 04:54:37 +0100

    working test contract

--- contract/contracts/Human.sol
+++ contract/contracts/Human.sol
@@ -4,30 +4,58 @@ pragma solidity ^0.8.10;
 
 import {AreYouHuman} from "./AreYouHuman.sol";
 
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction. While these are generally available
+ * via msg.sender, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
 abstract contract Context {
   function _msgSender() internal view virtual returns (address) {
     return msg.sender;
   }
 }
 
+/**
+ * @dev Contract module which provides a basic humanity check mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions only if identity verification is passed.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyHuman`, which can be applied to your functions to restrict unverified users.
+ */
 contract Human is Context {
-  address constant consumerContract =
-    0xf3ee05925Ffcd122930C870d7c0FB5E9cc86F489; // fillup
-  AreYouHuman consumer = AreYouHuman(consumerContract);
+  // const CONSUMER_CONTRACT: the api consumer contract
+  address constant CONSUMER_CONTRACT =
+    0xf3ee05925Ffcd122930C870d7c0FB5E9cc86F489;
+  AreYouHuman consumer = AreYouHuman(CONSUMER_CONTRACT);
 
+  /**
+   * @dev Throws if called by any account that has not verified identity.
+   */
   modifier onlyHuman() {
     _isVerified();
     _;
   }
 
+  /**
+   * @dev Throws if the sender verification status is not `approved`.
+   */
   function _isVerified() internal view virtual {
-    require(isVerified() == true, "has not pass human verification");
+    require(
+      (keccak256(bytes("approved")) == keccak256(bytes(getStatus()))) == true,
+      "human verification required!"
+    );
   }
 
-  function isVerified() public view returns (bool) {
-    string memory status = "approved";
-    string memory _status = consumer.getVerificationStatus(_msgSender());
-    if (keccak256(bytes(_status)) == keccak256(bytes(status))) return true;
-    return false; // return the verification status
+  /**
+   * @dev returns verification status of the sender from the consumer contract.
+   */
+  function getStatus() internal view returns (string memory) {
+    return consumer.getVerificationStatus(_msgSender());
   }
 }
